f056786eeeb10176c1d1952bd6c60a99d7e48494
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-19 19:05:35 +0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,31 +16,29 @@ export const Footer: FC<FooterProps> = () => {
               alt='logo'
             />
           </div>
-  
-          <div className={styles.FooterData}>
-            <div className={styles.Copyright}>
-              &copy; 2022 C A T T I A - Business center, technology transfer and business incubator
-            </div>
 
-            <div className={styles.ContactInfo}>
-              <div className={styles.Link}>
-                <img src='icon-email.png' alt=''/> cattia@metropolabrasov.ro
-              </div>
-              <div className={styles.Link}>
-                <img src='icon-tel.png' alt=''/>tel:+40268547616
-              </div>
-              <div className={styles.Link}>
-                <img src='icon-location.png' alt=''/>https://goo.gl/maps/cSq9skRUm82z1ouh6
-              </div>
-            </div>
+          <div className={styles.Copyright}>
+            &copy; 2022 C A T T I A - Business center, technology transfer and business incubator
+          </div>
 
-            <div className={styles.Media}>
-              <div className={styles.MediaIcon}>
-                <img src='icon-facebook.png' alt='' />
-              </div>
-              <div className={styles.MediaIcon}>
-                <img src='icon-youtube.png' alt='' />
-              </div>
+          <div className={styles.ContactInfo}>
+            <div className={styles.Link}>
+              <img src='icon-email.png' alt=''/> cattia@metropolabrasov.ro
+            </div>
+            <div className={styles.Link}>
+              <img src='icon-tel.png' alt=''/>tel:+40268547616
+            </div>
+            <div className={styles.Link}>
+              <img src='icon-location.png' alt=''/>https://goo.gl/maps/cSq9skRUm82z1ouh6
+            </div>
+          </div>
+
+          <div className={styles.Media}>
+            <div className={styles.MediaIcon}>
+              <img src='icon-facebook.png' alt='' />
+            </div>
+            <div className={styles.MediaIcon}>
+              <img src='icon-youtube.png' alt='' />
             </div>
           </div>
         </div>
