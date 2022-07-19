4491b3429c83aacdaf567c632e420a279976b1ea
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-19 21:10:58 +0300

    add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,20 @@
+import { FC, MouseEvent } from 'react'
+import styles from './Button.module.css'
+import cn from 'classnames'
+
+interface ButtonProps {
+  children: string
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void
+  outlined?: boolean
+}
+
+export const Button: FC<ButtonProps> = ({ children, onClick, outlined }) => {
+  return (
+    <button 
+      onClick={onClick} 
+      className={cn(styles.Button, outlined && styles.Outlined)}
+    >
+      {children}
+    </button>
+  )
+}
