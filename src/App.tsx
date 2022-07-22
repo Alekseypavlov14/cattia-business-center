0f3b46418bf575345a517f869242e1720a357f48
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-22 19:24:02 +0300

    add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
-import { Facilities } from './pages/Facilities/Facilities'
 import { Home } from './pages/Home/Home'
+import { Facilities } from './pages/Facilities/Facilities'
+import { Contacts } from './pages/Contacts/Contacts'
 
 export function App() {
   return (
@@ -12,9 +13,7 @@ export function App() {
         <Route path='/' element={<Home />} />
         <Route path='/presentation' element={<div />} />
         <Route path='/facilities' element={<Facilities />} />
-        <Route path='/rates' element={<div />} />
-        <Route path='/gallery' element={<div />} />
-        <Route path='/contact' element={<div />} />
+        <Route path='/contact' element={<Contacts />} />
       </Routes>
       <Footer />
     </>
